c2e2f53be5ad7fbfe60706c9d2ac301cd9f44396
evg1n <evginserbest@outlook.com>
2018-12-16 10:41:00 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import './index.css';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import { searchRobots } from './reducers.js';
+
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(thunkMiddleware,logger));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+registerServiceWorker();
